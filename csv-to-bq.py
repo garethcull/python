93e6453eb2a2bf5e7b28ea9dbf556008bed9f5d9
Gareth Cull <mail@garethcull.com>
2018-04-26 10:30:51 -0400

    Initial commit

--- csv-to-bq.py
+++ csv-to-bq.py
@@ -10,9 +10,8 @@ projectid = [INSERT PROJECT ID]
 # Read file into a DataFrame: df
 df = pd.read_csv(url)
 
-
 # create new table in BQ, if table already exists, replace it.
-gbq.to_gbq(df, [INSERT DATESET.TABLE], projectid,private_key=[INSERT_KEY],if_exists='replace')
+gbq.to_gbq(df, [INSERT DATASET.TABLE], projectid,private_key=[INSERT_KEY],if_exists='replace')
 
-
+#print the head() of the df
 print(df.head())
