f4b887b58372b3e18f49561dfd74784b57495e13
Gareth Cull <mail@garethcull.com>
2018-04-26 10:28:19 -0400

    Initial commit

---
+++ csv-to-bq.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pandas_gbq as gbq
+
+# Insert your BigQuery Project ID Here
+projectid = [INSERT PROJECT ID]
+
+# csv url
+#url = [INSERT URL TO PULL DATA]
+
+# Read file into a DataFrame: df
+df = pd.read_csv(url)
+
+
+# create new table in BQ, if table already exists, replace it.
+gbq.to_gbq(df, [INSERT DATESET.TABLE], projectid,private_key=[INSERT_KEY],if_exists='replace')
+
+
+print(df.head())
