6472604c38325249091db2278f508ce22414589e
Gareth Cull <mail@garethcull.com>
2018-06-19 21:05:16 -0400

    Updating comments

--- csv-to-bq.py
+++ csv-to-bq.py
@@ -13,5 +13,5 @@ df = pd.read_csv(url)
 # create new table in BQ, if table already exists, replace it.
 gbq.to_gbq(df, [INSERT DATASET.TABLE], projectid,private_key=[INSERT_KEY],if_exists='replace')
 
-#print the head() of the df
+#print the head() of df
 print(df.head())
